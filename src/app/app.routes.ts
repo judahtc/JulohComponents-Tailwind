72435a269c8e20cb5f5660f8bceeaf5c332ba22e
judahtc <jaycea247@gmail.com>
2024-09-24 16:16:44 +0200

    added cards components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ButtonComponent } from './button/button.component';
 import { InputsComponent } from './inputs/inputs.component';
+import { CardComponent } from './card/card.component';
 
 export const routes: Routes = [
   { path: 'button', component: ButtonComponent },
   { path: 'inputs', component: InputsComponent },
+  { path: 'cards', component: CardComponent },
 ];
