b5bbb0abc968bc46c5d888513d28ae8e0e7123ba
judahtc <jaycea247@gmail.com>
2024-09-24 15:36:31 +0200

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ButtonComponent } from './button/button.component';
+import { InputsComponent } from './inputs/inputs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'button', component: ButtonComponent },
+  { path: 'inputs', component: InputsComponent },
+];
