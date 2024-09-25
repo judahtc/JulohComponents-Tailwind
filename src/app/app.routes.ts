fad3c8c1d8e1c1eaf836363d892031126e739ee9
judahtc <jaycea247@gmail.com>
2024-09-25 10:17:33 +0200

    feat:added the fourth card to the cards component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,15 @@ import { InputsComponent } from './inputs/inputs.component';
 import { CardComponent } from './card/card.component';
 
 export const routes: Routes = [
+  {
+    path: 'portal',
+    component: ButtonComponent,
+    children: [
+      { path: 'button', component: ButtonComponent },
+      { path: 'inputs', component: InputsComponent },
+      { path: 'cards', component: CardComponent },
+    ],
+  },
   { path: 'button', component: ButtonComponent },
   { path: 'inputs', component: InputsComponent },
   { path: 'cards', component: CardComponent },
