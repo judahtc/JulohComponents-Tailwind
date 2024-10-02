7e5287632a38bb48e220e09e527d9237108312f3
judahtc <jaycea247@gmail.com>
2024-10-02 10:49:44 +0200

    added top nav and sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { ButtonComponent } from './button/button.component';
 import { InputsComponent } from './inputs/inputs.component';
 import { CardComponent } from './card/card.component';
+import { PortalComponent } from './portal/portal.component';
 
 export const routes: Routes = [
   {
     path: 'portal',
-    component: ButtonComponent,
+    component: PortalComponent,
     children: [
       { path: 'button', component: ButtonComponent },
       { path: 'inputs', component: InputsComponent },
