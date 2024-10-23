499b8120314354e9b25bfffbc89123cd9558eadb
judahtc <jaycea247@gmail.com>
2024-10-23 08:17:57 +0200

    created a data table

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-table',
   standalone: true,
   imports: [],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.css'
+  styleUrl: './table.component.css',
 })
-export class TableComponent {
-
+export class TableComponent implements OnInit {
+  data: any;
+  ngOnInit(): void {
+    this.data = [
+      {
+        id: 1,
+        component: 'button',
+        items: 4,
+        description: 'various button types with various classes',
+      },
+      {
+        id: 2,
+        component: 'card',
+        items: 5,
+        description: 'various card types with various classes',
+      },
+      {
+        id: 3,
+        component: 'inputs',
+        items: 6,
+        description: 'various input types with various classes',
+      },
+      ,
+      {
+        id: 4,
+        component: 'reg forms',
+        items: 9,
+        description: 'various reg forms types with various classes',
+      },
+      ,
+      {
+        id: 5,
+        component: 'login',
+        items: 6,
+        description: 'various login form types with various classes',
+      },
+      ,
+      {
+        id: 6,
+        component: 'links',
+        items: 6,
+        description: 'various link types with various classes',
+      },
+      ,
+      {
+        id: 7,
+        component: 'top navs',
+        items: 3,
+        description: 'various nav types with various classes',
+      },
+    ];
+  }
 }
