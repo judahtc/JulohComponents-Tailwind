59e21360fe0fb682c056c094985ad27175dbce21
judahtc <jaycea247@gmail.com>
2024-10-24 10:42:54 +0200

    feat: add initial version of a pagination

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
       boxShadow: {
         "inner-right": "inset -5px 0 10px rgba(0, 0, 0, 0.2)",
       },
+
+      fontFamily: {
+        sans: [
+          "inter",
+          "ui-serif",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Arial",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+        ],
+      },
     },
   },
   plugins: [],
