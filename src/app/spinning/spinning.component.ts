ba7df372ee3595d9bd1bb48080541b2fd9b99b55
judahtc <jaycea247@gmail.com>
2024-11-07 21:17:48 +0200

    created a spinning component

---
+++ src/app/spinning/spinning.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spinning',
+  standalone: true,
+  imports: [],
+  templateUrl: './spinning.component.html',
+  styleUrl: './spinning.component.css'
+})
+export class SpinningComponent {
+
+}
